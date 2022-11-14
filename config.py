785569ebafbe53afe981b0b92ffd692c89c6d995
GhostOne01 <118037214+GhostOne01@users.noreply.github.com>
2022-11-13 16:55:54 -1200

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ API_ID =  os.environ.get('api_id','18641760')
 API_HASH = os.environ.get('api_hash','b7b026ce9d1d36400c02dc21d8df53a3')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','valdes_95').split(';')
-#ACCES_USERS = ('tl_admin_user','valdes_95')
+ACCES_USERS = os.environ.get('tl_admin_user','GhostOne01').split(';')
+#ACCES_USERS = ('tl_admin_user','GhostOne01')
 #ACCES_USERS = os.environ.get(ACCES_USERS)
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','socks5h://KHDGKDYGJDJFGJYHJHGKGEYIKGGGDERJCDLEFHLE'))
 
